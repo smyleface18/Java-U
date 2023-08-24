b8a700b185d4b6fd2e3d43178beb83396f98f3b5
smyleface18 <ca30850@gmail.com>
2023-08-24 13:24:47 -0500

    loader java

---
+++ src/main/java/com/mycompany/javau/JavaU.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javau;
+
+/**
+ *
+ * @author caleb
+ */
+public class JavaU {
+
+    public static void main(String[] args) {
+        int number1,number2;
+        number1=2; number2=4;
+        
+        System.out.println("la suma es: " + (number1+number2));
+        
+    }
+}
