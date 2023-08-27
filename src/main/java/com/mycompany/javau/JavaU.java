ca66f357410b021365b3abfd044e1b06c13cdda4
smyleface18 <30850@gmail.com>
2023-08-27 12:23:31 -0500

    ejercicios 1 termiando

--- src/main/java/com/mycompany/javau/JavaU.java
+++ src/main/java/com/mycompany/javau/JavaU.java
@@ -57,5 +57,91 @@ public class JavaU {
         System.out.println("La nota del estudiante es: "+(((final_score/3)*0.7)+(exam*0.3)));
         */
         
-    }
+        /*
+        int age,container_age;
+        container_age=0;
+        for (int i =0; i<7; i++){
+            Scanner read= new Scanner(System.in);
+            System.out.println("Introduzca la edad del estudiante: ");
+            age=read.nextInt();
+            container_age=container_age+age; 
+        }     
+        System.out.println("La edad promedio de los estuidantes es de: "+(container_age/7));   
+        */
+        
+        /*
+        double dolar,peso;
+        Scanner read=new Scanner (System.in);
+        System.out.println("Ingrese el valor del dolar en pesos: ");
+        peso=read.nextDouble();
+        System.out.println("Ingrese la cantidad de dolares: ");
+        dolar=read.nextDouble();
+        System.out.println("La cantidad de pesos que posee en dolares es de: "+ (peso*dolar)+"$");
+        */
+        
+        /*
+        double precio;
+        final double iva=1.19;
+        Scanner read=new Scanner (System.in);
+        System.out.println("Introduzca el precio base del producto:");
+        precio=read.nextDouble();
+        System.out.println("El precio final del producto es de: "+(precio*iva)+"$");
+        */
+        
+        /*
+        double person1,person2,person3,person4,person5,person6;
+        Scanner read=new Scanner(System.in);
+        System.out.println("Introduzca la cantidad que aporte la persona 1: ");
+        person1=read.nextDouble();
+        System.out.println("Introduzca la cantidad que aporte la persona 2: ");
+        person2=read.nextDouble();
+        System.out.println("Introduzca la cantidad que aporte la persona 3: ");
+        person3=read.nextDouble();
+        System.out.println("Introduzca la cantidad que aporte la persona 4: ");
+        person4=read.nextDouble();
+        System.out.println("Introduzca la cantidad que aporte la persona 5: ");
+        person5=read.nextDouble();
+        System.out.println("Introduzca la cantidad que aporte la persona 6: ");
+        person6=read.nextDouble();
+        
+            if (person1>person2 & person1>person3 & person1>person4 & person1>person5 & person1>person6)
+            {
+                System.out.println("la persona 1 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+            if (person2>person1 & person2>person3 & person2>person4 & person2>person5 & person2>person6)
+            {
+                System.out.println("la persona 2 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+            if (person3>person1 & person3>person2 & person3>person4 & person3>person5 & person3>person6)
+            {
+                System.out.println("la persona 3 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+            if (person4>person1 & person4>person2 & person4>person3 & person4>person5 & person4>person6)
+            {
+                System.out.println("la persona 4 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+            if (person5>person1 & person5>person2 & person5>person3 & person5>person4 & person5>person6)
+            {
+                System.out.println("la persona 5 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+            if (person6>person1 & person6>person2 & person6>person3 & person6>person4 & person6>person5)
+            {
+                System.out.println("la persona 6 recibira el 50% del premio y las otras personas se repartiran el otro 50%");
+            }
+             */   
+                
+                
+                
+                
+                /*
+                int number;
+                Scanner read= new Scanner(System.in);
+                System.out.println("Introduzca un numero: ");
+                number=read.nextInt();
+                System.out.println("///////////////////////");
+                for (int i=0;i<=10;i++){
+                System.out.println(number*i);
+                }
+                */
+            }
 }
