db1eeb8710abcce410700b7be41470a65073ca70
smyleface18 <ca30850@gmail.com>
2023-08-26 22:06:49 -0500

    avanzando

--- src/main/java/com/mycompany/javau/JavaU.java
+++ src/main/java/com/mycompany/javau/JavaU.java
@@ -8,12 +8,54 @@ package com.mycompany.javau;
  *
  * @author caleb
  */
+import java.util.Scanner;
 public class JavaU {
 
     public static void main(String[] args) {
-        int work_days;
+        
+        /*int work_days;
         work_days= 23;
         final int day_value=2000;
         System.out.println("El valor a pagar es: "+(work_days*day_value));
+        */
+        
+        /*
+        double heigth, weigth;
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Introduzca la altura en metros: ");
+        heigth=leer.nextDouble();
+        System.out.println("Introduzca el peso kilogramos: ");
+        weigth=leer.nextDouble();
+        System.out.println("El indice de masa corporal es de: "+(weigth/(heigth*heigth)));
+        */
+        
+        /*
+        double heigth,base;
+        Scanner leer= new Scanner(System.in);
+        System.out.println("Introduzca la altura del lote en metros: ");
+        heigth=leer.nextDouble();
+        System.out.println("Introduzca la base del lote en metros: ");
+        base=leer.nextDouble();
+        System.out.println("El area del lote es: "+(base*heigth)+" m^2");
+        */
+        
+        /*
+        double score,exam,final_score;
+        final_score=0;
+        Scanner leer=new Scanner (System.in);
+        System.out.println("Introduzca la nota 1: ");
+        score=leer.nextDouble();
+        final_score=final_score+score;
+        System.out.println("Introduzca la nota 2: ");
+        score=leer.nextDouble();
+        final_score=final_score+score;
+        System.out.println("Introduzca la nota 3: ");
+        score=leer.nextDouble();
+        final_score=final_score+score;
+        System.out.println("Introduzca la nota del examen: ");
+        exam=leer.nextDouble();
+        System.out.println("La nota del estudiante es: "+(((final_score/3)*0.7)+(exam*0.3)));
+        */
+        
     }
 }
